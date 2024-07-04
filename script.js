ef896e521849b1cda66c63ab5374158efeb02eba
sdeep98 <sdeependra1234@gmail.com>
2024-07-04 11:54:28 +0530

    home page complete without responsive

---
+++ script.js
@@ -0,0 +1,6 @@
+var menu = document.querySelector(".menu ul")
+var menuIcon = document.querySelector(".menu i")
+
+menuIcon.addEventListener("click", function() {
+  menu.style.display = "initial"
+})
