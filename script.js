ecdcd1debcd112d3c73a26fdfe310fceaab3234d
sdeep98 <sdeependra1234@gmail.com>
2024-07-04 18:02:17 +0530

    front page add responsive

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var menu = document.querySelector(".menu ul")
-var menuIcon = document.querySelector(".menu i")
+var menu = document.querySelector(".menu")
+var menuIcon = document.querySelector(".nav i")
+var cross = document.querySelector(".menu i")
 
 menuIcon.addEventListener("click", function() {
-  menu.style.display = "initial"
+  menu.style.top = 0
+})
+
+cross.addEventListener("click", function() {
+  menu.style.top = "-10%"
 })
